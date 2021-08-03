8f418b80f9a38d4c3b707bcf6106c3b614d3626c
Vinu Vaghasia <vinu.vaghasia@amd.com>
2021-08-03 15:10:52 -0500

    recipes-amd: Set Fan Speed Script modified to support multiple boards
    1. Script reads boadr_id from u-boot env and sets number of i2c bus accordingly
    2. Script read the enumarated i2c bus behind i2c-9 mux from i2cdetect cmd.
    3. Then sets speed for all avaialalbe Fans at 50%
    
    Signed-off-by: Vinu Vaghasia <vinu.vaghasia@amd.com>
    Change-Id: Iceffca264b148dd9eb51e85c4de5221d0e74ee5e

--- meta-amd/meta-sp5/recipes-amd/set-fan-speed/files/set-fan-speed.sh
+++ meta-amd/meta-sp5/recipes-amd/set-fan-speed/files/set-fan-speed.sh
@@ -2,18 +2,38 @@
 
 set -e
 
-# Controller Dev ID
+# Fan Controller Dev ID
 EMC2305_DEV=0x4D
 
-#Fan speed control Reg
-FAN1_SET_REG=0x30
-FAN2_SET_REG=0x40
-FAN3_SET_REG=0x50
-FAN4_SET_REG=0x60
-FAN5_SET_REG=0x70
+#Speed Limit
+SPEED_LIMIT=0X80
 
-# Speed Limit
-SPEED_LIMIT=0x80
+#Fan speed control Regs, for 1-5 fans
+FAN_SET_REG=("0x30" "0x40" "0x50" "0x60" "0x70")
+num_of_fans=${#FAN_SET_REG[@]}
+
+
+# Read i2c bus list from i2cdetect cmd
+mapfile -t i2c_bus_array < <( i2cdetect -l | grep i2c-9-mux | cut -c 5-6 | sort )
+
+# Count no of i2c buses
+num_of_i2c_bus=${#i2c_bus_array[@]}
+
+# Read board_id from u-boot env
+board_id=`/sbin/fw_printenv -n board_id`
+
+# Onyx board
+if [[ "$board_id" == "40" ]] || [[ "$board_id" == "41" ]] || [[ "$board_id" == "42" ]]; then
+    num_of_fan_bus=4
+elif [[ "$board_id" == "43" ]] || [[ "$board_id" == "44" ]] || [[ "$board_id" == "45" ]]; then
+    num_of_fan_bus=5
+elif [[ "$board_id" == "46" ]] || [[ "$board_id" == "47" ]] || [[ "$board_id" == "48" ]]; then
+    num_of_fan_bus=5
+elif [[ "$board_id" == "49" ]] || [[ "$board_id" == "4A" ]] || [[ "$board_id" == "4B" ]]; then
+    num_of_fan_bus=5
+else
+	num_of_fan_bus=5
+fi
 
 # Check host power status
 power_status() {
@@ -25,36 +45,36 @@ power_status() {
     fi
 }
 
-
 # Set Fan speeds
 set_fan_speed()
 {
     # Wrtie speed valu to controller Regs.
     echo "Setting all Fan speeds to $speed_val"
     echo
-    for i in 16 17 18 19
+
+    for ((i=0; i<${num_of_fan_bus}; i++));
     do
-        i2cset -y $i $EMC2305_DEV $FAN1_SET_REG $speed_val
-        i2cset -y $i $EMC2305_DEV $FAN2_SET_REG $speed_val
-        i2cset -y $i $EMC2305_DEV $FAN3_SET_REG $speed_val
-        i2cset -y $i $EMC2305_DEV $FAN4_SET_REG $speed_val
-        i2cset -y $i $EMC2305_DEV $FAN5_SET_REG $speed_val
+        for (( j=0; j <${num_of_fans}; j++));
+        do
+            i2cset -y ${i2c_bus_array[i]} $EMC2305_DEV ${FAN_SET_REG[j]} $speed_val
+        done
     done
     exit 0
 }
 
 # Verify input speed is not below Limit val.
 if [[ $1 -lt $SPEED_LIMIT ]]; then
-        echo "Error : You can not set Fan speed less then 50% (0x80)"
-        exit -1
+    echo "Error : You can not set Fan speed less then 50% (0x80)"
+    exit -1
 else
-        speed_val=$1
+    speed_val=$1
 fi
 
 
 sleep 10
 while :
 do
+    # Check host power status, if 'off' then wait until power gets 'on'
     if [[ $(power_status) != "off" ]];
     then
         set_fan_speed
@@ -63,6 +83,3 @@ do
         sleep 10
     fi
 done
-
-
-
