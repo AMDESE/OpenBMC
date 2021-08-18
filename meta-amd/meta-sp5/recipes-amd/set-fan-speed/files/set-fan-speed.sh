3a4afec3e66b74fe7e7440f5833705b000d29234
Sreedhar Govardhana <srgovard@amd.com>
2021-08-18 17:22:16 -0500

    recipes-amd: Mux enablement for APML
    This patch will set the IMX3112 mux registers to enable access to APML. Added the
    required logic in set-fan-speed.sh script.
    
    Signed-off-by: Sreedhar Govardhana <srgovard@amd.com>
    Change-Id:  If9f5b6a6167481f702cd9ec7320b0f7e1258fd2e

--- meta-amd/meta-sp5/recipes-amd/set-fan-speed/files/set-fan-speed.sh
+++ meta-amd/meta-sp5/recipes-amd/set-fan-speed/files/set-fan-speed.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+# Read board_id from u-boot env
+board_id=`/sbin/fw_printenv -n board_id`
+
+# APML BEGIN
+# NOTE: Adding some APML functionality to this script.
+# Though, it is not fan related, using it for time being
+# as this change may short lived (until I3C enablement).
+I2C_BUS=2
+I2C_MUX=0x70
+I2C_MUX_MR64=0x40 #MUX port sel
+I2C_MUX_MR65=0x41 #MUX port rw enable
+
+while [ $I2C_BUS -le 3 ]
+do
+    #set I2C_MUX MR64 reg to APML - set bit 6 '1'
+    i2cset -y $I2C_BUS $I2C_MUX $I2C_MUX_MR64 0x40 >& /dev/null
+    if [[ $? != 0 ]]
+    then
+        echo "Error: APML mux setting on $I2C_BUS failed (MR64)"
+        break
+    fi
+
+    #set I2C_MUX MR65 reg to enable rw access- set bit 6 '1'
+    i2cset -y $I2C_BUS $I2C_MUX $I2C_MUX_MR65 0x40 >& /dev/null
+    if [[ $? != 0 ]]
+    then
+        echo "Error: APML mux setting on $I2C_BUS failed (MR65)"
+        break
+    fi
+
+    #for Quartz we need to do the same for P1 APML too
+    if [[ "$board_id" == "43" ]] || [[ "$board_id" == "44" ]] || [[ "$board_id" == "45" ]]; then
+        ((I2C_BUS++))
+    else
+        break
+    fi
+done
+
+# APML END - REMOVE APML BEGIN to END once enabling I3C
+
 set -e
 
 # Fan Controller Dev ID
@@ -19,9 +59,6 @@ mapfile -t i2c_bus_array < <( i2cdetect -l | grep i2c-9-mux | cut -c 5-6 | sort 
 # Count no of i2c buses
 num_of_i2c_bus=${#i2c_bus_array[@]}
 
-# Read board_id from u-boot env
-board_id=`/sbin/fw_printenv -n board_id`
-
 # Onyx board
 if [[ "$board_id" == "40" ]] || [[ "$board_id" == "41" ]] || [[ "$board_id" == "42" ]]; then
     num_of_fan_bus=4
@@ -32,7 +69,7 @@ elif [[ "$board_id" == "46" ]] || [[ "$board_id" == "47" ]] || [[ "$board_id" ==
 elif [[ "$board_id" == "49" ]] || [[ "$board_id" == "4A" ]] || [[ "$board_id" == "4B" ]]; then
     num_of_fan_bus=5
 else
-	num_of_fan_bus=5
+    num_of_fan_bus=5
 fi
 
 # Check host power status
