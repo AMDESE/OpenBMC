9e60a6a8ec5cc5f6459ae49b2a48e309b67ed618
Vinu Vaghasia <vinu.vaghasia@amd.com>
2021-10-10 16:25:49 -0500

    recipes-amd: modify set-fan-speed script for lowering the fan speed
    set-fan-speed is modified for:
      a. To set all the fans at 30% duty cycle at the boot time.
      b. To set the  Pump fans at 100% duty cycle.
      c. Service file is modified to run as early as possible.
    
    Signed-off-by: Vinu Vaghasia <vinu.vaghasia@amd.com>
    Change-Id: Id1bc1d7581e92deca4b5caaf4338ad6e8698a49c

--- meta-amd/meta-sp5/recipes-amd/set-fan-speed/files/set-fan-speed.sh
+++ meta-amd/meta-sp5/recipes-amd/set-fan-speed/files/set-fan-speed.sh
@@ -15,16 +15,16 @@ I2C_MUX_MR65=0x41 #MUX port rw enable
 while [ $I2C_BUS -le 3 ]
 do
     #set I2C_MUX MR64 reg to APML - set bit 6 '1'
-    i2cset -y $I2C_BUS $I2C_MUX $I2C_MUX_MR64 0x40 >& /dev/null
-    if [[ $? != 0 ]]
+    i2cset -f -y $I2C_BUS $I2C_MUX $I2C_MUX_MR64 0x40 >& /dev/null
+    if [[ $? -ne 0 ]]
     then
         echo "Error: APML mux setting on $I2C_BUS failed (MR64)"
         break
     fi
 
     #set I2C_MUX MR65 reg to enable rw access- set bit 6 '1'
-    i2cset -y $I2C_BUS $I2C_MUX $I2C_MUX_MR65 0x40 >& /dev/null
-    if [[ $? != 0 ]]
+    i2cset -f -y $I2C_BUS $I2C_MUX $I2C_MUX_MR65 0x40 >& /dev/null
+    if [[ $? -ne 0 ]]
     then
         echo "Error: APML mux setting on $I2C_BUS failed (MR65)"
         break
@@ -45,13 +45,12 @@ set -e
 # Fan Controller Dev ID
 EMC2305_DEV=0x4D
 
-#Speed Limit
-SPEED_LIMIT=0X80
+#Speed Limit 20%
+SPEED_LIMIT=0X32
 
 #Fan speed control Regs, for 1-5 fans
 FAN_SET_REG=("0x30" "0x40" "0x50" "0x60" "0x70")
 num_of_fans=${#FAN_SET_REG[@]}
-
 
 # Read i2c bus list from i2cdetect cmd
 mapfile -t i2c_bus_array < <( i2cdetect -l | grep i2c-9-mux | cut -c 5-6 | sort )
@@ -59,64 +58,78 @@ mapfile -t i2c_bus_array < <( i2cdetect -l | grep i2c-9-mux | cut -c 5-6 | sort 
 # Count no of i2c buses
 num_of_i2c_bus=${#i2c_bus_array[@]}
 
-# Onyx board
-if [[ "$board_id" == "40" ]] || [[ "$board_id" == "41" ]] || [[ "$board_id" == "42" ]]; then
-    num_of_fan_bus=4
-elif [[ "$board_id" == "43" ]] || [[ "$board_id" == "44" ]] || [[ "$board_id" == "45" ]]; then
-    num_of_fan_bus=5
-elif [[ "$board_id" == "46" ]] || [[ "$board_id" == "47" ]] || [[ "$board_id" == "48" ]]; then
-    num_of_fan_bus=5
-elif [[ "$board_id" == "49" ]] || [[ "$board_id" == "4A" ]] || [[ "$board_id" == "4B" ]]; then
-    num_of_fan_bus=5
-else
-    num_of_fan_bus=5
-fi
+# Simply Hack it and default to 25 Fans
+num_of_emc2305_controller=5
 
-# Check host power status
-power_status() {
-    st=$(busctl get-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis CurrentPowerState | cut -d"." -f6)
-    if [[ "$st" == "On\"" ]]; then
-        echo "on"
-    else
-        echo "off"
-    fi
-}
+#existing no of emc2305 controller
+curr_emc2305_ctrl=0;
 
-# Set Fan speeds
+fail=0
+
+# Set all Fans speeds at argument passed by user
 set_fan_speed()
 {
-    # Wrtie speed valu to controller Regs.
-    echo "Setting all Fan speeds to $speed_val"
-    echo
-
-    for ((i=0; i<${num_of_fan_bus}; i++));
+    # Write speed value to emc2305 controller Regs.
+    echo "Setting all Fan speeds to $speed_val pwm"
+    for ((i=0; i<${num_of_emc2305_controller}; i++));
     do
         for (( j=0; j <${num_of_fans}; j++));
         do
-            i2cset -y ${i2c_bus_array[i]} $EMC2305_DEV ${FAN_SET_REG[j]} $speed_val
+            i2cset -f -y ${i2c_bus_array[i]} $EMC2305_DEV ${FAN_SET_REG[j]} $speed_val || retval=$?
+        if [[ "$retval" -ne 0 ]]; then
+            echo "Error: Setting fan speed failed or there is no Fan connected..."
+            fail=1
+            break
+        fi
         done
     done
-    exit 0
+    if [[ $fail -eq 1 ]]; then
+       curr_emc2305_ctrl=$((i-1))
+    else
+       curr_emc2305_ctrl=${i}
+    fi
+
 }
 
-# Verify input speed is not below Limit val.
+# Set all Pump fans at full speed
+set_pump_fan_speed()
+{
+   speed_val=0xFF
+   echo "Number of emc2305 controllers: ${curr_emc2305_ctrl}"
+   if [[ $curr_emc2305_ctrl -eq 4 ]]; then
+        # Set Onyx pump fan speed
+        # Pump fan is on emc2305 controller# 3, fan#2
+        echo "Setting Onyx Pump Fans at full speed...."
+        i2cset -f -y ${i2c_bus_array[2]} $EMC2305_DEV ${FAN_SET_REG[1]} $speed_val
+        if [ $? -ne 0 ]; then
+            echo "Error: Setting Pump fan speed failed.."
+        fi
+   elif [[ $curr_emc2305_ctrl -eq 5 ]]; then
+        # Set Quartz pump fan speed
+        # Pump fan is on emc2305 controller# 5, fan# 4 & 5
+        echo "Setting Quartz Pump Fans at full speed...."
+        for (( j=3; j < 5; j++ ));
+        do
+            i2cset -f -y ${i2c_bus_array[4]} $EMC2305_DEV ${FAN_SET_REG[j]} $speed_val
+            if [ $? -ne 0 ]; then
+                echo "Error: Setting Pump fan speed failed.."
+            fi
+        done
+   fi
+}
+
+
+# Main()
+#---------
+
+# Verify that input speed is not below Limit value.
 if [[ $1 -lt $SPEED_LIMIT ]]; then
-    echo "Error : You can not set Fan speed less then 50% (0x80)"
+    echo "Error : You can not set Fan speed less then 20% (0x32)"
     exit -1
 else
     speed_val=$1
 fi
 
-
-sleep 10
-while :
-do
-    # Check host power status, if 'off' then wait until power gets 'on'
-    if [[ $(power_status) != "off" ]];
-    then
-        set_fan_speed
-        exit 0
-    else
-        sleep 10
-    fi
-done
+# Call functions to set Fan speeds
+set_fan_speed
+set_pump_fan_speed
