7b160c764ff8382b66d402637d57838425dfce28
Mohsen Dolaty <mohsen.dolaty@amd.com>
2022-03-11 09:53:27 -0600

    amd-apml:i3c setup with SBTSI
    for I3C setup, to avoid i3c driver crash, 1st unbind SBTSI driver
    Also if I3C Mux already exist then do not run discovery of the i3c Mux
    
    Signed-off-by: Mohsen Dolaty <mohsen.dolaty@amd.com>
    Change-Id: I9e49488b96edae3c67d6280461f4cdb66b164f1d

--- meta-amd/meta-sp5/recipes-amd/amd-apml/files/set-apml.sh
+++ meta-amd/meta-sp5/recipes-amd/amd-apml/files/set-apml.sh
@@ -17,6 +17,15 @@ DEV_I3C_4="1e7a6000.i3c4"
 DEV_I3C_5="1e7a7000.i3c5"
 I3C_MUX_0="/dev/i3c-4-4cc00000000"
 I3C_MUX_1="/dev/i3c-5-4cc00000000"
+i3c_mux_present=0
+
+DEV_I3C_SBTSI_PATH="/sys/bus/i3c/drivers/sbtsi_i3c"
+DEV_I3C_SBTSI_4="4-22400000001"
+DEV_I3C_SBTSI_5="5-22400000001"
+
+# DEV_I3C_SBRMI_PATH="/sys/bus/i3c/drivers/sbrmi_i3c"
+# DEV_I3C_SBRMI_4="4-22400000002"
+# DEV_I3C_SBRMI_5="5-22400000002"
 
 # check num of cpu
 check_num_of_cpu()
@@ -83,29 +92,88 @@ set_i2c_apml()
 set_i3c_apml()
 {
     echo "Setting I3C Mux for CPU 0 APML "
-    echo  $DEV_I3C_4 > $DEV_I3C_PATH/unbind
-    echo  $DEV_I3C_4 > $DEV_I3C_PATH/bind
-    sleep 1
-
+    if [ -a "$I3C_MUX_0" ]
+    then
+        $I3C_TOOL -d $I3C_MUX_0 -w 0x46 -r 1 > /dev/null 2>&1
+        if [[ $? -ne 0 ]]
+        then
+            echo "I3C Mux 0 not present"
+            i3c_mux_present=0
+        else
+            echo "I3C Mux 0 present"
+            i3c_mux_present=1
+        fi
+    else
+        i3c_mux_present=0
+    fi
+    if [[ $i3c_mux_present == 0 ]]
+    then
+        # Unbind sbtsi driver
+        echo  $DEV_I3C_SBTSI_4 > $DEV_I3C_SBTSI_PATH/unbind
+        # echo  $DEV_I3C_SBRMI_4 > $DEV_I3C_SBRMI_PATH/unbind
+        sleep 1
+        # Unbind platform i3c driver
+        echo  $DEV_I3C_4 > $DEV_I3C_PATH/unbind
+        sleep 1
+        # Bind platform i3c driver
+        echo  $DEV_I3C_4 > $DEV_I3C_PATH/bind
+        sleep 1
+    fi
     $I3C_TOOL -d $I3C_MUX_0 -w 0x46,0x00,0x01
     $I3C_TOOL -d $I3C_MUX_0 -w 0x40,0x00,0x40
     $I3C_TOOL -d $I3C_MUX_0 -w 0x41,0x00,0x40
 
+    # Unbind sbtsi driver
+    echo  $DEV_I3C_SBTSI_4 > $DEV_I3C_SBTSI_PATH/unbind
+    # echo  $DEV_I3C_SBRMI_4 > $DEV_I3C_SBRMI_PATH/unbind
+    sleep 1
+    # Unbind platform i3c driver
     echo  $DEV_I3C_4 > $DEV_I3C_PATH/unbind
+    sleep 1
+    # Bind platform i3c driver
     echo  $DEV_I3C_4 > $DEV_I3C_PATH/bind
 
     if [[ $num_of_cpu == 2 ]]
     then
         echo "Setting I3C Mux for CPU 1 APML "
-        echo  $DEV_I3C_5 > $DEV_I3C_PATH/unbind
-        echo  $DEV_I3C_5 > $DEV_I3C_PATH/bind
-        sleep 1
-
+        if [ -a "$I3C_MUX_1" ]
+        then
+            $I3C_TOOL -d $I3C_MUX_1 -w 0x46 -r 1 > /dev/null 2>&1
+            if [[ $? -ne 0 ]]
+            then
+                echo "I3C Mux 1 not present"
+                i3c_mux_present=0
+            else
+                echo "I3C Mux 1 present"
+                i3c_mux_present=1
+            fi
+        else
+            i3c_mux_present=0
+        fi
+        if [[ $i3c_mux_present == 0 ]]
+        then
+            # Unbind sbtsi driver
+            echo  $DEV_I3C_SBTSI_5 > $DEV_I3C_SBTSI_PATH/unbind
+            # echo  $DEV_I3C_SBRMI_5 > $DEV_I3C_SBRMI_PATH/unbind
+            sleep 1
+            # Unbind platform i3c driver
+            echo  $DEV_I3C_5 > $DEV_I3C_PATH/unbind
+            sleep 1
+            # Bind platform i3c driver
+            echo  $DEV_I3C_5 > $DEV_I3C_PATH/bind
+            sleep 1
+        fi
         $I3C_TOOL -d $I3C_MUX_1 -w 0x46,0x00,0x01
         $I3C_TOOL -d $I3C_MUX_1 -w 0x40,0x00,0x40
         $I3C_TOOL -d $I3C_MUX_1 -w 0x41,0x00,0x40
-
+        # Unbind sbtsi driver
+        echo  $DEV_I3C_SBTSI_5 > $DEV_I3C_SBTSI_PATH/unbind
+        # echo  $DEV_I3C_SBRMI_5 > $DEV_I3C_SBRMI_PATH/unbind
+        sleep 1
+        # Unbind platform i3c driver
         echo  $DEV_I3C_5 > $DEV_I3C_PATH/unbind
+        sleep 1
+        # Bind platform i3c driver
         echo  $DEV_I3C_5 > $DEV_I3C_PATH/bind
     fi
 }
