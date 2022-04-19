d10de9d90bfc923b401e572c72c944e642c7fd16
Rajaganesh Rathinasabapathi <Rajaganesh.Rathinasabapathi@amd.com>
2022-04-19 07:49:45 -0500

    recipes-amd/vr-update: Add support for RAA229620
    Adds support for renesas part number RAA229620 used in Titanite.
    
    Signed-off-by: Rajaganesh Rathinasabapathi <Rajaganesh.Rathinasabapathi@amd.com>
    Change-Id: I8706b33ca4841a48444ca8d600c7f1cdce561926

--- meta-amd/meta-sp5/recipes-amd/vr-update/files/renesas-vr-update.sh
+++ meta-amd/meta-sp5/recipes-amd/vr-update/files/renesas-vr-update.sh
@@ -19,6 +19,26 @@ echo "Renesas VR upgrade started at $(date)"
 echo "$PROCESSOR $ADDR $HEX_FILE $MODEL_NUMBER $BOARD"
 
 echo "Renesas VR update image is $HEX_FILE"
+PLAT=`amd-plat-info get_board_info name`
+if [ "$PLAT" == "titanite" ] ; then
+    echo "Titanite detected"
+    if [ "$PROCESSOR" == "P0" ] ; then
+        BUS=5
+    elif [ "$PROCESSOR" == "P1" ] ; then
+        BUS=6
+    fi
+    MODEL_NUMBER="RAA229620"
+
+    /usr/bin/vr-update $BUS $ADDR $HEX_FILE $MODEL_NUMBER -b $BOARD
+    if [ "$?" -ne "0" ];
+    then
+        echo "VR upgrade has failed\n"
+        exit -1
+    else
+        echo "VR upgrade has succeeded! Please AC cycle for the changes to take effect.\n"
+    fi
+    exit 0
+fi
 BUS=`find /sys/bus/i2c/drivers/isl68137/ -name "*$ADDR" -exec basename {} \; | cut -d- -f 1`
 P0=${BUS:0:2}
 P1=${BUS:2:4}
