2eb07cac553c1ca2cf677a623505fd60c75fc355
Abinaya <abinaya.dhandapani@amd.com>
2022-04-21 01:19:36 -0500

    recipes/recipes-phosphor:Add VR Support for Infineon TDA parts
    Adds support for Infineon TDA VR upgrade (.xsf file format).
    
    Signed-off-by: Abinaya <abinaya.dhandapani@amd.com>
    Change-Id: I2c91f154557de64fc5a7971ebb2e9a8b3c6f82fb

--- meta-amd/meta-sp5/recipes-phosphor/flash/phosphor-software-manager/vr-update.sh
+++ meta-amd/meta-sp5/recipes-phosphor/flash/phosphor-software-manager/vr-update.sh
@@ -3,10 +3,10 @@
 # VR upgrade
 # Below are the BMC GPIO settings to access VR SPI flash
 # GPIOs: [VR_BMC_GPIOO1:VR_BMC_GPIOO0]
-#   	[00] = Run Mode
-#   	[01] = Local BIOS SPI
-#   	[10] = HPM FPGA SPI
-#   	[11] = HPM LOM SPI
+#       [00] = Run Mode
+#       [01] = Local BIOS SPI
+#       [10] = HPM FPGA SPI
+#       [11] = HPM LOM SPI
 #
 
 set +e
@@ -28,17 +28,18 @@ else
     exit -1
 fi
 
-IMAGE_FILE=$(find -type f -name '*.hex')
-
-if [ ! -e "$IMAGE_FILE" ]; then
+if [ -e *.hex ] ; then
+    IMAGE_FILE=$(find -type f -name '*.hex')
+elif [ -e *.bin ] ; then
     IMAGE_FILE=$(find -type f -name '*.bin')
-    if [ ! -e "$IMAGE_FILE" ]; then
-        IMAGE_FILE=$(find -type f -name '*.txt')
-		if [ ! -e "$IMAGE_FILE" ]; then
-        	echo "VR image file does not exist"
-        	exit -1
-		fi
-    fi
+elif [ -e *.txt ] ; then
+    IMAGE_FILE=$(find -type f -name '*.txt')
+elif [ -e *.xsf ] ; then
+    IMAGE_FILE=$(find -type f -name '*.xsf')
+    slave_addr=$(sed '3!d' $MANIFEST_FILE | awk -F= '{print $NF}')
+else
+    echo "VR image file does not exist"
+    exit -1
 fi
 
 echo "VR image file is $IMAGE_FILE"
@@ -55,12 +56,17 @@ if [ "$Manufacturer" == "Renesas" ]; then
     fi
 
 elif [ "$Manufacturer" == "Infineon" ]; then
-    /usr/bin/infineon-vr-update.sh $IMAGE_FILE $Manufacturer
-    if [ $? -eq 0 ]; then
-        echo "Infineon VR update Successful"
+    IMAGE_FILE=$(find -type f -name '*.xsf')
+    if [ -e "$IMAGE_FILE" ]; then
+        /usr/bin/infineon-vr-update.sh $IMAGE_FILE $Manufacturer $slave_addr
     else
-        echo "Infineon VR update failed"
-        exit -1
+        /usr/bin/infineon-vr-update.sh $IMAGE_FILE $Manufacturer
+        if [ $? -eq 0 ]; then
+            echo "Infineon VR update Successful"
+        else
+            echo "Infineon VR update failed"
+            exit -1
+        fi
     fi
 else
     echo "Not a valid Manufacturer Name. Aborting the update"
