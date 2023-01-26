27a19a8276b0bd1279327bdace791f4b58c4d614
Mohsen Dolaty <mohsen.dolaty@amd.com>
2023-01-26 17:26:12 -0600

    set-fan-speed: Add Turin Platforms
    Turin Platforms use 2 different HWMON
    Lenovo platforms use NCT7362
    Other platforms use EMC2305
    Both are supported with the new script file
    
    Signed-off-by: Mohsen Dolaty <mohsen.dolaty@amd.com>
    Change-Id: Ice885f9bc8ff843a3c2372ae254321ef2f6d88ca

--- meta-amd/meta-turin/recipes-amd/set-fan-speed/files/set-fan-speed.sh
+++ meta-amd/meta-turin/recipes-amd/set-fan-speed/files/set-fan-speed.sh
@@ -4,23 +4,39 @@ set -e
 
 # Fan Controller Dev ID
 EMC2305_DEV=0x4D
+NCT7362_DEV=0x20
 CPLD_DEV=0x28
+
+curr_emc2305_ctrl=0;
 
 #Speed Limit 20% (Range 0x0 to 0xFF)
 SPEED_LIMIT=0x32
 
-#Fan speed control Regs, for 1-5 fans
-FAN_SET_REG=("0x30" "0x40" "0x50" "0x60" "0x70")
-num_of_fans=${#FAN_SET_REG[@]}
+set_emc2305()
+{
+    #Fan speed control Regs, for 1-5 fans
+    FAN_SET_REG=("0x30" "0x40" "0x50" "0x60" "0x70")
+    num_of_fans=${#FAN_SET_REG[@]}
 
-# Read i2c bus list from i2cdetect cmd
-mapfile -t i2c_bus_array < <(find /sys/bus/i2c/drivers | grep emc2301 | grep 004d | cut -d"/" -f 7 | cut -d"-" -f 1 | sort)
+    # Read i2c bus list from i2cdetect cmd
+    mapfile -t i2c_bus_array < <(find /sys/bus/i2c/drivers | grep emc2301 | grep 004d | cut -d"/" -f 7 | cut -d"-" -f 1 | sort)
 
-# Get the number of emc2305 controllers
-num_of_emc2305_controller=${#i2c_bus_array[@]}
+    # Get the number of emc2305 controllers
+    num_of_emc2305_controller=${#i2c_bus_array[@]}
+}
 
-#existing no of emc2305 controller
-curr_emc2305_ctrl=0;
+set_nct7362()
+{
+    #Fan speed control Regs, for 1-5 fans
+    FAN_SET_REG=("0x90" "0xA0")
+    num_of_fans=${#FAN_SET_REG[@]}
+
+    # Read i2c bus list from i2cdetect cmd
+    mapfile -t i2c_bus_array < <(find /sys/bus/i2c/drivers | grep nct7362 | grep 0020 | cut -d"/" -f 7 | cut -d"-" -f 1 | sort)
+
+    # Get the number of nct7362 controllers
+    num_of_nct7362_controller=${#i2c_bus_array[@]}
+}
 
 # Set all Fans speeds at argument passed by user
 set_emc2305_fan_speed()
@@ -41,6 +57,29 @@ set_emc2305_fan_speed()
     done
 
     curr_emc2305_ctrl=${i}
+}
+
+set_nct7362_fan_speed()
+{
+    echo "Number of NCT7362 controller = ${num_of_nct7362_controller}"
+    # Write speed value to nct7362 controller Regs.
+    echo "Setting all Fan speeds to $speed_val pwm"
+    for ((i=0; i<${num_of_nct7362_controller}; i++));
+    do
+        printf " Current NCT7362 Bus %s \n" ${i2c_bus_array[i]}
+        if [[ "$i" -eq 1 ]]; then
+            speed_val=0xFF
+            echo " Set Pump Fan to Full speed "
+        fi
+        for (( j=0; j <${num_of_fans}; j++));
+        do
+            i2cset -f -y ${i2c_bus_array[i]} $NCT7362_DEV ${FAN_SET_REG[j]} $speed_val || retval=$?
+        if [[ "$retval" -ne 0 ]]; then
+            echo "Error: Setting fan speed failed or there is no Fan connected..."
+            break
+        fi
+        done
+    done
 }
 
 # Set all Pump fans at full speed
@@ -69,31 +108,6 @@ set_emc2305_pump_fan_speed()
    fi
 }
 
-set_cpld_fan_speed()
-{
-    #Fan speed control Regs 6 fans
-    CPLD_FAN_SET_REG=("0x18" "0x19" "0x1A" "0x1B" "0x1C" "0x1D")
-    cpld_num_of_pwms=${#CPLD_FAN_SET_REG[@]}
-    echo " Num of pwms = ${cpld_num_of_pwms}"
-    # Get the CPLD i2c bus number
-    cpld_i2c_bus_num=`find /sys/bus/i2c/drivers | grep cpld| grep 0028 | cut -d"/" -f 7 | cut -d"-" -f 1`
-    echo "cpld i2c bus num = ${cpld_i2c_bus_num}"
-
-    # Write speed value to CPLD controller Regs.
-    # Set speed to 30% pwm, (CPLD range 0 to 100 %)
-    pwm_val=30
-    echo "Setting all Huambo Fan speeds to $pwm_val pwm"
-    for ((i=0; i<${cpld_num_of_pwms}; i++));
-    do
-        i2cset -f -y ${cpld_i2c_bus_num} $CPLD_DEV ${CPLD_FAN_SET_REG[i]} $pwm_val || retval=$?
-        if [[ "$retval" -ne 0 ]]; then
-            echo "Error: Setting fan speed failed or there is no Fan connected..."
-            break
-        fi
-    done
-}
-
-
 # Main()
 #---------
 
@@ -101,18 +115,29 @@ set_cpld_fan_speed()
 board_id=`fw_printenv board_id | sed -n "s/^board_id=//p"`
 echo "board_id = ${board_id}"
 
-if [[ $board_id == "67" ]]; then
-    set_cpld_fan_speed
+# Verify that input speed is not below Limit value.
+if [[ $1 -lt $SPEED_LIMIT ]]; then
+    echo "Error : You can not set Fan speed less then 20% (0x32)"
+    exit -1
 else
-    # Verify that input speed is not below Limit value.
-    if [[ $1 -lt $SPEED_LIMIT ]]; then
-        echo "Error : You can not set Fan speed less then 20% (0x32)"
-        exit -1
-    else
-        speed_val=$1
-    fi
+    speed_val=$1
+fi
 
-    # Call functions to set EMC2305  Fan speeds
+case "$board_id" in
+    "6A" | "6a" | "72" | "73")
+    echo "Set Purico Fans "
+    set_nct7362
+    set_nct7362_fan_speed
+    ;;
+    "6B" | "6b" | "74" | "75")
+    echo "Set Volcano Fans "
+    set_nct7362
+    set_nct7362_fan_speed
+    ;;
+    *)
+    echo "Set Turin Fans "
+    set_emc2305
     set_emc2305_fan_speed
     set_emc2305_pump_fan_speed
-fi
+    ;;
+esac
